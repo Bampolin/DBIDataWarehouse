daa37dfaa4c7a7a779cfea0b9ea937d00e45ac2a
Bampolin <87871034+Bampolin@users.noreply.github.com>
2024-04-03 14:44:29 +0200

    Controller Anfang

--- src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
+++ src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
@@ -1,7 +1,45 @@
 package com.example.datawarehouse.controller;
 
+import com.example.datawarehouse.domain.Customer;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/salesfacts")
 public class DataWarehouseController {
+
+    @PostMapping
+    public ResponseEntity<?> saveFact(
+            @RequestParam(required = false) Long customerID,
+            @RequestParam(required = false) String customerName,
+            @RequestParam(required = false) String customerCity,
+            @RequestParam(required = false) String customerStreet,
+            @RequestParam(required = false) Integer customerPLZ,
+            @RequestParam(required = false) String customerCountry,
+
+            @RequestParam(required = false) Long productID,
+            @RequestParam(required = false) String productName,
+            @RequestParam(required = false) String productCategory,
+            @RequestParam(required = false) Double productBuyingPrice,
+
+            @RequestParam(required = false) Long storeID,
+            @RequestParam(required = false) String storeCity,
+            @RequestParam(required = false) String storeStreet,
+            @RequestParam(required = false) String storePLZ,
+            @RequestParam(required = false) String storeCountry,
+
+            @RequestParam(required = false) String salesDate,
+            @RequestParam(required = false) Integer quantity,
+            @RequestParam(required = false) Double price
+    ) {
+        Customer customer = new Customer();
+
+
+
+
+        return null;
+    }
+
+
+
 }
--- src/main/java/com/example/datawarehouse/service/CustomerService.java
+++ src/main/java/com/example/datawarehouse/service/CustomerService.java
@@ -5,6 +5,7 @@ import com.example.datawarehouse.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,7 +26,9 @@ public class CustomerService {
         return customerRepository.findById(id);
     }
 
-
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
 
 
 }
