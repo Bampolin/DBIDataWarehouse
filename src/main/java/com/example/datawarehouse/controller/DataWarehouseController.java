ffa523dfe963d4844a5c735777f67aaf28d95dbb
Bampolin <87871034+Bampolin@users.noreply.github.com>
2024-04-03 14:50:26 +0200

    setter im Controller verwenden für Übergabewerte

--- src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
+++ src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
@@ -1,6 +1,9 @@
 package com.example.datawarehouse.controller;
 
 import com.example.datawarehouse.domain.Customer;
+import com.example.datawarehouse.domain.Product;
+import com.example.datawarehouse.domain.SalesDate;
+import com.example.datawarehouse.domain.Store;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +28,7 @@ public class DataWarehouseController {
             @RequestParam(required = false) Long storeID,
             @RequestParam(required = false) String storeCity,
             @RequestParam(required = false) String storeStreet,
-            @RequestParam(required = false) String storePLZ,
+            @RequestParam(required = false) Integer storePLZ,
             @RequestParam(required = false) String storeCountry,
 
             @RequestParam(required = false) String salesDate,
@@ -33,6 +36,28 @@ public class DataWarehouseController {
             @RequestParam(required = false) Double price
     ) {
         Customer customer = new Customer();
+        Product product = new Product();
+        SalesDate salesDateObj = new SalesDate();
+        Store store = new Store();
+
+        customer.setOltpID(customerID);
+        customer.setName(customerName);
+        customer.setCity(customerCity);
+        customer.setPlz(customerPLZ);
+        customer.setStreet(storeStreet);
+        customer.setStreet(customerStreet);
+        customer.setCountry(customerCountry);
+
+        product.setOltpID(productID);
+        product.setName(productName);
+        product.setCategory(productCategory);
+        product.setBuyingPrice(productBuyingPrice);
+
+        store.setOltpID(storeID);
+        store.setCity(storeCity);
+        store.setPlz(storePLZ);
+        store.setCountry(storeCountry);
+        store.setStreet(storeStreet);
 
 
 
