5bd9a47590186c1fb899a2504d331def3b4c1018
Bampolin <87871034+Bampolin@users.noreply.github.com>
2024-04-03 14:20:32 +0200

    packages

---
+++ src/main/java/com/example/datawarehouse/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.datawarehouse.repository;
+
+import com.example.datawarehouse.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
