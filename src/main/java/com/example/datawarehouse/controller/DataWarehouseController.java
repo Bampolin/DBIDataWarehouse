233438376780602b46ee254f05fd3f85c2b211f1
20190608 <k.baier@htlstp.at>
2024-04-08 11:57:38 +0200

    changed fact table and Controller to JSON

--- src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
+++ src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
@@ -1,9 +1,6 @@
 package com.example.datawarehouse.controller;
 
-import com.example.datawarehouse.domain.Customer;
-import com.example.datawarehouse.domain.Product;
-import com.example.datawarehouse.domain.SalesDate;
-import com.example.datawarehouse.domain.Store;
+import com.example.datawarehouse.domain.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,53 +8,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/salesfacts")
 public class DataWarehouseController {
 
-    @PostMapping
+    @PostMapping("/save")
     public ResponseEntity<?> saveFact(
-            @RequestParam(required = false) Long customerID,
-            @RequestParam(required = false) String customerName,
-            @RequestParam(required = false) String customerCity,
-            @RequestParam(required = false) String customerStreet,
-            @RequestParam(required = false) Integer customerPLZ,
-            @RequestParam(required = false) String customerCountry,
-
-            @RequestParam(required = false) Long productID,
-            @RequestParam(required = false) String productName,
-            @RequestParam(required = false) String productCategory,
-            @RequestParam(required = false) Double productBuyingPrice,
-
-            @RequestParam(required = false) Long storeID,
-            @RequestParam(required = false) String storeCity,
-            @RequestParam(required = false) String storeStreet,
-            @RequestParam(required = false) Integer storePLZ,
-            @RequestParam(required = false) String storeCountry,
-
-            @RequestParam(required = false) String salesDate,
-            @RequestParam(required = false) Integer quantity,
-            @RequestParam(required = false) Double price
+            @RequestBody SalesFacts salesFacts
     ) {
+
         Customer customer = new Customer();
         Product product = new Product();
         SalesDate salesDateObj = new SalesDate();
         Store store = new Store();
 
-        customer.setOltpID(customerID);
-        customer.setName(customerName);
-        customer.setCity(customerCity);
-        customer.setPlz(customerPLZ);
-        customer.setStreet(storeStreet);
-        customer.setStreet(customerStreet);
-        customer.setCountry(customerCountry);
-
-        product.setOltpID(productID);
-        product.setName(productName);
-        product.setCategory(productCategory);
-        product.setBuyingPrice(productBuyingPrice);
-
-        store.setOltpID(storeID);
-        store.setCity(storeCity);
-        store.setPlz(storePLZ);
-        store.setCountry(storeCountry);
-        store.setStreet(storeStreet);
 
 
 
