44fa08a2423ad3efc4f152e156b4fbc6b7b30fbf
20190608 <k.baier@htlstp.at>
2024-04-08 12:00:25 +0200

    changed request body

--- src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
+++ src/main/java/com/example/datawarehouse/controller/DataWarehouseController.java
@@ -4,19 +4,22 @@ import com.example.datawarehouse.domain.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/salesfacts")
 public class DataWarehouseController {
 
     @PostMapping("/save")
     public ResponseEntity<?> saveFact(
-            @RequestBody SalesFacts salesFacts
-    ) {
+            @RequestBody List<SalesFacts> salesFacts
+            ) {
 
         Customer customer = new Customer();
         Product product = new Product();
         SalesDate salesDateObj = new SalesDate();
         Store store = new Store();
+
 
 
 
